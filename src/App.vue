343857a186c71e6a5532e671f5822b82d9dea7e8
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-07-27 20:45:12 -0300

    feat: adicionando lógica

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+//criando constantes dos numeros e definindo elas como 0
+const num1 = ref(0);
+const num2 = ref(0);
+const selectedOperation = ref('adicao');
+
+//criando funções das operações aritmeticas
+function soma() {
+  return num1.value + num2.value;
+}
+
+function subtrai() {
+  return num1.value - num2.value;
+}
+
+function divide() {
+  if (num2.value === 0) {
+    return "Erro: Divisão por zero";
+  }
+  return num1.value / num2.value;
+}
+
+function multiplicao() {
+  return num1.value * num2.value;
+}
+
+//relacionando as funções com o select
+function resultado() {
+  switch (selectedOperation.value) { 
+    case 'adicao':
+      return soma();
+    case 'subtracao':
+      return subtrai();
+    case 'divisao':
+      return divide();
+    case 'multiplicacao':
+      return multiplicao();
+    default:
+      return '';
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <header class="p-5 bg-light rounded-3">
+      <h1>Calculadora com Vue</h1>
+    </header>
+    <form class="p-5">
+      <div class="form-row">
+        <div class="col">
+          <input class="form-control" required type="number" placeholder="Digite um número" v-model="num1">
+        </div>
+        <div class="col mt-4">
+          <input class="form-control" required type="number" placeholder="Digite um número" v-model="num2">
+        </div>
+        <div class="col-md-2">
+          <select class="form-control mt-4" v-model="selectedOperation">
+            <option value="adicao">Adição</option>
+            <option value="subtracao">Subtração</option>
+            <option value="divisao">Divisão</option>
+            <option value="multiplicacao">Multiplicação</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <div class="p-5">
+      <p>{{ resultado() }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
